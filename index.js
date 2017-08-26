45fc12399f37135112fb97317a73d587968ccf4b
RAXAAXXAXAXAXAXXAXAXAXAXXAXAXAXAXLLLLLLLLLIÓÒÓÒÓÒÓÒ´´O``´`´`´´´´`´Ò´`´`´`O <ramxaxlon@gmail.com>
2017-08-26 17:40:37 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
+require("socket.io").listen(
 require("http").createServer((req,res)=>{
 res.writeHead(200,{"content-type":"text/html"})
 res.write("Hello world!")
 res.end()
 }).listen(process.env.PORT || 3000)
+).on("connection",(window)=>{
+
+})
