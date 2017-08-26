ec5929deb2f204d51cb84393233ae2bd31cf7378
RAXAAXXAXAXAXAXXAXAXAXAXXAXAXAXAXLLLLLLLLLIÓÒÓÒÓÒÓÒ´´O``´`´`´´´´`´Ò´`´`´`O <ramxaxlon@gmail.com>
2017-08-26 21:39:21 +0200

    Update index.js

--- index.js
+++ index.js
@@ -12,17 +12,19 @@ obj[window.id]={x:32,y:32}
 window.emit('msg',{script:'k=[]'})
 window.emit('msg',{script:'document.body.style.margin="0"'})
 window.emit('msg',{script:'onkeyup=onkeydown=(e)=>{k[e.keyCode]=e.type="keydown"}'})
-window.emit('msg',{script:'setInterval(()=>{for(i=0;i<400;i++){if(k[i]){io().emit(i)}}},1)'})
+window.emit('msg',{script:'setInterval(()=>{for(i=0;i<400;i++){if(k[i]){io().emit(i)}}},1000)'})
 window.emit('msg',{script:'c=document.createElement("canvas")'})
 window.emit('msg',{script:'document.body.appendChild(c)'})
 window.emit('msg',{script:'ctx=c.getContext("2d")'})
-window.on('37',()=>{obj[window.id].x-=1})
-window.on('38',()=>{obj[window.id].y-=1})
-window.on('39',()=>{obj[window.id].x+=1})
-window.on('40',()=>{obj[window.id].y+=1})
+window.on('37',()=>{window.emit('msg',{script:'console.log("Pressed left key!")'}))
+window.on('38',()=>{window.emit('msg',{script:'console.log("Pressed up key!")'}))
+window.on('39',()=>{window.emit('msg',{script:'console.log("Pressed right key!")'}))
+window.on('40',()=>{window.emit('msg',{script:'console.log("Pressed down key!")'}))
+/*
 setInterval(()=>{
 window.emit('msg',{script:'console.clear()'})
 for(i in obj){
 window.emit('msg',{script:'console.log("'+obj[i].x+'")'})
 }},1000)
+*/
 })
