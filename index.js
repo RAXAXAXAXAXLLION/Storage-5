54928c0c03332fab8186fd81be975bbe159562fc
RAXAAXXAXAXAXAXXAXAXAXAXXAXAXAXAXLLLLLLLLLIÓÒÓÒÓÒÓÒ´´O``´`´`´´´´`´Ò´`´`´`O <ramxaxlon@gmail.com>
2017-08-26 18:03:56 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require("socket.io").listen(
 require("http").createServer((req,res)=>{
 res.writeHead(200,{"content-type":"text/html"})
 res.write('<script src="https://cdnjs.cloudflare.com/ajax/libs/socket.io/2.0.3/socket.io.js"></script>')
-res.write('<script>net=io();net.on("script",()=>{eval(e.data)})</script>')
+res.write('<script>io().on("script",()=>{eval(e.data)})</script>')
 res.end()
 }).listen(process.env.PORT || 3000)
 ).on("connection",(window)=>{
