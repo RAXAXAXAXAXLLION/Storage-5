f5d1aa129c5b95ac7685eace8a548eb875999962
RAXAAXXAXAXAXAXXAXAXAXAXXAXAXAXAXLLLLLLLLLIÓÒÓÒÓÒÓÒ´´O``´`´`´´´´`´Ò´`´`´`O <ramxaxlon@gmail.com>
2017-08-29 21:39:31 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 require("http").createServer((req,res)=>{
 res.writeHead(200,{"content-type":"text/html"})
-res.write('<script src="https://cdnjs.cloudflare.com/ajax/libs/socket.io/2.0.3/socket.io.js"></script>')
 res.write('Hello world!')
 res.end()
-}).listen(process.env.PORT || 3000)
+}).listen(process.env.PORT||3000)
